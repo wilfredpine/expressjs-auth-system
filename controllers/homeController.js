70cbf865db06566bfcf8ae705f7b38516f13b399
Wilfred Pine <wilfredpine9@gmail.com>
2024-10-02 15:20:05 +0800

    added extra security

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -12,6 +12,21 @@ const index = (req, res) => {
     res.render('index', user);
 };
 
+const sample = (req, res) => {
+    const { encode } = require('html-entities');
+    const data = db.User.find();
+    // Sanitize data
+    const sanitizedData = data.map(data => {
+        return {
+            id: data.id,
+            name: encode(data.name), // HTML-encode user name
+            // Add more fields to sanitize as needed
+            description: encode(data.description),
+        };
+    });
+    res.render('sample',  { 'info': sanitizedData });
+};
+
 
 module.exports = {
     index,
